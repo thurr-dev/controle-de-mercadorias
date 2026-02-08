09a739284b94b72b601ba42731694d652fd15e72
thurr-dev <arthurfern89@gmail.com>
2026-02-08 08:50:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'adega-cache-v1';
+const CACHE_NAME = 'adega-cache-v2';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './style.css', './script.js', './manifest.json', './icone2.png'])));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
+
